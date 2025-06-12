e44da866d774b3ae8423d957b59d42b6e0fea588
Rachmat Rizki <rahmatrizki089@gmail.com>
2025-06-12 12:18:31 +0700

    add: crud permission

--- src/main/java/com/axa/simple_auth_axa_req/service/impl/PermissionServiceImpl.java
+++ src/main/java/com/axa/simple_auth_axa_req/service/impl/PermissionServiceImpl.java
@@ -2,6 +2,7 @@ package com.axa.simple_auth_axa_req.service.impl;
 
 import com.axa.simple_auth_axa_req.dto.permission.PermissionDTO;
 import com.axa.simple_auth_axa_req.dto.permission.UpsertPermissionDTO;
+import com.axa.simple_auth_axa_req.exception.NotFoundException;
 import com.axa.simple_auth_axa_req.model.Permission;
 import com.axa.simple_auth_axa_req.repository.PermissionRepository;
 import com.axa.simple_auth_axa_req.service.PermissionService;
@@ -27,16 +28,34 @@ public class PermissionServiceImpl implements PermissionService {
 
     @Override
     public List<PermissionDTO> getAllPermissions() {
-        return List.of();
+        log.info("req all permissions");
+
+        return permissionRepository.findAll().stream().map(PermissionServiceImpl::mapToDto).toList();
     }
 
     @Override
     public PermissionDTO upsertPermission(Long permissionId, UpsertPermissionDTO dto) {
-        return null;
+        log.info("req upsert permission");
+        Permission foundPermission = permissionRepository.findById(permissionId)
+                                                         .orElseGet(() -> Permission.builder()
+                                                                                    .type(dto.getType())
+                                                                                    .build());
+
+        if (permissionId == 0) {
+            foundPermission.setType(dto.getType());
+        }
+
+        return mapToDto(permissionRepository.save(foundPermission));
     }
 
     @Override
     public void deletePermission(Long permissionId) {
+        log.info("req delete permission: {}", permissionId);
 
+        if (!permissionRepository.existsById(permissionId)) {
+            throw new NotFoundException("Permission not found", null);
+        }
+
+        permissionRepository.deleteById(permissionId);
     }
 }
